aa968436abf9fddb301fbe404342b6c776d75bf5
555shivv <shivanisarvan5@gmail.com>
2024-12-24 14:28:33 +0530

    modified index.js

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,10 @@ const cors = require('cors');
 const compilex = require('compilex');
 
 const app = express();
-app.use(cors()); // Add this line to enable CORS
+//app.use(cors()); // Add this line to enable CORS
 app.use(express.json());
+app.use(cors({ origin: 'https://helpful-dusk-1267f0.netlify.app/' }));
+
 
 // Compilex initialization
 
@@ -20,6 +22,9 @@ const upload = multer({ dest: 'temp/' });
 
 app.post('/compile', upload.single('code'), (req, res) => {
     const filePath = req.file.path;
+    if(!req.file){
+        return res.status(400).json({ message: 'No file uploaded' });
+    }
     const envData = { OS: 'windows', cmd: 'g++',options: {timeout: 5000} };
 
     fs.readFile(filePath, 'utf8', (err, code) => {
