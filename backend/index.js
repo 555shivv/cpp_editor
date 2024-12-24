ba2d58bb61cfb8b4f5194c990fbde883d761c57d
555shivv <shivanisarvan5@gmail.com>
2024-12-24 15:04:30 +0530

    port issue fixed

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.post('/compile', upload.single('code'), (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
